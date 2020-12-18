8a16e84f7fdba64b884c276e14c03fb5802da3db
Damon <damonspencerroberts@gmail.com>
2020-12-18 20:30:56 +0100

    put data to json

--- src/Components/Posts/Posts.js
+++ src/Components/Posts/Posts.js
@@ -1,65 +1,33 @@
 import React from 'react';
 import classes from './Posts.module.css';
 
-const Posts = () => {
+const Posts = (props) => {
 
+    const data = props.jsondata;
+
+    const DifferentPosts = data.map((e, i) => {
+        return (
+            <div key = {i} className = {classes.Posts}>
+                <div className = {classes.Title}>
+                    <h3>{e.title}</h3>
+                </div>
+                <p>{e.content.slice(0, 160)}...</p>
+                <div className = {classes.Author}>
+                    <p>{e.author}</p>
+                </div>
+                <div className = {classes.Dates}>
+                    <p>{e.date}</p>
+                </div>
+            </div>
+        );
+    });
     return(
         <div className = {classes.List}>
             <div className = {classes.Header}>
                 <h1>Previous Posts</h1>
             </div>
             
-            <div className = {classes.Posts}>
-                <div className = {classes.Title}>
-                    <h3>The wonderful world of Jameson</h3>
-                </div>
-                <p>Preview for the wonderful that begins with a nice little story at the edge of a cliff. It was dark that night when young danny boy decided to go up the mount...</p>
-                <div className = {classes.Author}>
-                    <p>Robert Buchanan</p>
-                </div>
-                <div className = {classes.Dates}>
-                    <p>11/03/2020</p>
-                </div>
-            </div>
-            
-            <div className = {classes.Posts}>
-                <div className = {classes.Title}>
-                    <h3>The wonderful world of Jameson</h3>
-                </div>
-                <p>Preview for the wonderful that begins with a nice little story at the edge of a cliff. It was dark that night when young danny boy decided to go up the mount...</p>
-                <div className = {classes.Author}>
-                    <p>Robert Buchanan</p>
-                </div>
-                <div className = {classes.Dates}>
-                    <p>11/03/2020</p>
-                </div>
-            </div>
-            
-            <div className = {classes.Posts}>
-                <div className = {classes.Title}>
-                    <h3>The wonderful world of Jameson</h3>
-                </div>
-                <p>Preview for the wonderful that begins with a nice little story at the edge of a cliff. It was dark that night when young danny boy decided to go up the mount...</p>
-                <div className = {classes.Author}>
-                    <p>Robert Buchanan</p>
-                </div>
-                <div className = {classes.Dates}>
-                    <p>11/03/2020</p>
-                </div>
-            </div>
-            
-            <div className = {classes.Posts}>
-                <div className = {classes.Title}>
-                    <h3>The wonderful world of Jameson</h3>
-                </div>
-                <p>Preview for the wonderful that begins with a nice little story at the edge of a cliff. It was dark that night when young danny boy decided to go up the mount...</p>
-                <div className = {classes.Author}>
-                    <p>Robert Buchanan</p>
-                </div>
-                <div className = {classes.Dates}>
-                    <p>11/03/2020</p>
-                </div>
-            </div>
+            {DifferentPosts}
         </div>
     );
 
--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {setState} from 'react';
 import Posts from "../Components/Posts/Posts";
 import IndividualPost from "../Components/Individual-Post/Post";
 import Logo from "../Components/Logo/Logo";
 import classes from "./Home.module.css";
+import JsonInfo from "../json-data/json-data";
 
 const Home = () => {
+    
     return (
         <div className = {classes.Container}>
             <div className = {classes.Div1}>
@@ -16,7 +18,9 @@ const Home = () => {
             </div>
 
             <div className = {classes.Div3}>
-                <Posts />
+                <Posts 
+                    jsondata = {JsonInfo}
+                />
             </div>
 
             <div className = {classes.Div4}>
