a85db4b04b0d4c4e77b6fab00fc0b62a545c36ff
Damon <damonspencerroberts@gmail.com>
2020-12-19 01:48:57 +0100

    added comp did mount

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component, useState } from 'react';
 import Posts from "../Components/Posts/Posts";
 import IndividualPost from "../Components/Individual-Post/Post";
 import Logo from "../Components/Logo/Logo";
@@ -6,48 +6,80 @@ import classes from "./Home.module.css";
 import NewPost from "../Components/New-Post/new-post";
 import JsonInfo from "../json-data/json-data";
 
-const Home = () => {
-    const [chosen, setChosen] = useState(JsonInfo[0]);
 
+class Home extends Component {
+    constructor() {
+        super()
 
+        this.state = {
+            chosen: JsonInfo[0]
+        }
 
-    const clickedBlog = (clickedIndex) => {
+        this.clickedBlog = this.clickedBlog.bind(this);
+    }
+
+    componentDidMount() {
+        axios.get("/blog-post.json")
+        .then(res => {
+            const s = [];
+            for (let e in res.data) {
+                s.push({
+                    ...res.data[e],
+                    id: e
+                });
+            }
+            
+           this.setState({dbdata: s, chosen: this.state.dbdata[0]});
+        })
+        .catch(err => {
+            alert(err);
+        })
+    }
+
+    clickedBlog(clickedIndex) {
         const s = JsonInfo.find((e, i) => {
-            return clickedIndex === i;
-        });
+                return clickedIndex === i;
+            });
+    
+        this.setState({chosen: s});
 
-        setChosen(s);
     }
     
-    return (
-        <div className = {classes.Container}>
-            <div className = {classes.Div1}>
-                <Logo />
-            </div>
+    render() {
+        return(
+            <div className = {classes.Container}>
+                <div className = {classes.Div1}>
+                    <Logo />
+                </div>
 
-            <div className = {classes.Div2}>
-                <IndividualPost 
-                    curpost = {chosen}
-                />
-            </div>
+                <div className = {classes.Div2}>
+                    <IndividualPost 
+                        curpost = {this.state.chosen}
+                    />
+                </div>
 
-            <div className = {classes.Div3}>
-                <Posts 
-                    jsondata = {JsonInfo}
-                    clickedBlog = {clickedBlog}
-                />
-            </div>
+                <div className = {classes.Div3}>
+                    <Posts 
+                        jsondata = {JsonInfo}
+                        clickedBlog = {this.clickedBlog}
+                    />
+                </div>
 
-            <div className = {classes.Div4}>
-                <NewPost />
-            </div>
+                <div className = {classes.Div4}>
+                    <NewPost />
+                </div>
 
-            <div className = {classes.Div5}>
-            
+                <div className = {classes.Div5}>
+                
+                </div>
             </div>
-        </div>
-        
-    );
+        );
+    }
 }
 
+
 export default Home;
+
+/**
+ * 
+ */
